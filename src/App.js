58c673b1db96b2f2778093a977fcdb0395d227b1
AndMed <andmed20@yandex.ru>
2022-01-29 15:23:53 +0300

    добавление basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='react-fake-api-store'>
       <Context>
         <div className='page'>
           <Header/>
